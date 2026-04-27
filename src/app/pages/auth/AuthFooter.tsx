b24b57f7c0abcd1c0c775d4b3297787c54fc1c00
mocki-toki <mocki.toki@gmail.com>
2026-04-27 21:44:58 +0300

    feat(branding): rename in-app product copy and links

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -5,20 +5,32 @@ import * as css from './styles.css';
 export function AuthFooter() {
   return (
     <Box className={css.AuthFooter} justifyContent="Center" gap="400" wrap="Wrap">
-      <Text as="a" size="T300" href="https://cinny.in" target="_blank" rel="noreferrer">
+      <Text
+        as="a"
+        size="T300"
+        href="https://github.com/mocki-toki/lumiere"
+        target="_blank"
+        rel="noreferrer"
+      >
         About
       </Text>
       <Text
         as="a"
         size="T300"
-        href="https://github.com/ajbura/cinny/releases"
+        href="https://github.com/mocki-toki/lumiere/releases"
         target="_blank"
         rel="noreferrer"
       >
         v4.11.1
       </Text>
-      <Text as="a" size="T300" href="https://twitter.com/cinnyapp" target="_blank" rel="noreferrer">
-        Twitter
+      <Text
+        as="a"
+        size="T300"
+        href="https://github.com/mocki-toki/lumiere"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
       </Text>
       <Text as="a" size="T300" href="https://matrix.org" target="_blank" rel="noreferrer">
         Powered by Matrix
