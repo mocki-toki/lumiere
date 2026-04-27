abc4a44938d724a0d59cf20c8a94ca0dc9d03aff
mocki-toki <mocki.toki@gmail.com>
2026-04-27 21:45:08 +0300

    chore(branding): update client-facing identifiers

--- src/app/features/settings/devices/LocalBackup.tsx
+++ src/app/features/settings/devices/LocalBackup.tsx
@@ -28,7 +28,7 @@ function ExportKeys() {
         const blob = new Blob([encKeys], {
           type: 'text/plain;charset=us-ascii',
         });
-        FileSaver.saveAs(blob, 'cinny-keys.txt');
+        FileSaver.saveAs(blob, 'lumiere-keys.txt');
       },
       [mx]
     )
